60f2b40357dd0aca4fff3620d87b5cf802c8fe86
Alexandre <alex.yam@outlook.fr>
2018-06-06 12:45:47 +0200

    book new done

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,4 +3,31 @@ class BooksController < ApplicationController
         @books = Book.all
     end
     
+    def new
+        @book = Book.new
+    end
+
+    def create
+        @book = Book.new(book_params)
+        respond_to do |format|
+            if @book.save
+                format.html { redirect_to books_path, notice: 'Your portfolio item is now live.' }
+            else
+                format.html { render :new }
+            end
+        end
+    end
+
+
+
+
+
+
+
+
+    private
+    def book_params
+        params.require(:book).permit(:title, :subtitle, :body)
+    end
+    
 end
