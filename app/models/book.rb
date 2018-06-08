1ecdb734f20e716d25205ed7823f7bcfdbf64fa0
Alexandre <alex.yam@outlook.fr>
2018-06-08 14:10:22 +0200

    auth done

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: books
+#
+#  id         :bigint(8)        not null, primary key
+#  title      :string
+#  subtitle   :string
+#  body       :text
+#  main_img   :text
+#  thumb_img  :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Book < ApplicationRecord
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: skills
+#
+#  id               :bigint(8)        not null, primary key
+#  title            :string
+#  percent_utilized :integer
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  badge            :text
+#
+
 class Skill < ApplicationRecord
 end
